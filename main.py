db7d610d7c998519d8aca4aa7b3069673e95a8e1
tim_green <tim.allan.green@gmail.com>
2025-07-29 22:17:27 +0930

    added character_count function

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ def get_book_text(file_path):
         return book.read()
 
 from stats import word_count
+from stats import character_count
 
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
     print(f"{word_count(book_text)} words found in the document")
+    print(character_count(book_text))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def word_count(book_text):
     words = book_text.split()
     return len(words)
+
+def character_count(book_text):
+    characters = {}
+    lower_case = book_text.lower()
+    for char in lower_case:
+        if char not in characters:
+            characters[char] = 1
+        else:
+            characters[char] +=1
+    return characters
+
+        
